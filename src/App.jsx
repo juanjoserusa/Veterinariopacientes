b13e104df992dc6eccadca3b3ad28bcf5a86cb81
Juanjo Ruiz <juanjoserusa@gmail.com>
2023-02-12 12:34:43 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import Formulario from './components/Formulario'
+import Header from './components/Header'
+import ListadoPacientes from './components/ListadoPacientes'
+
+
+function App() {
+
+  const [pacientes, setPacientes] = useState([]);
+
+  const[paciente, setPaciente ] =useState({});
+
+
+  return (
+    <div className="container mx-auto  mt-20">
+      <Header />
+
+      <div className='mt-12 md:flex'>
+      <Formulario 
+        pacientes={pacientes}
+        paciente={paciente}
+        setPacientes={setPacientes}
+      />
+      <ListadoPacientes 
+        pacientes={pacientes}
+        setPaciente={setPaciente}
+
+      />
+      </div>
+    </div>
+  )
+}
+
+export default App
